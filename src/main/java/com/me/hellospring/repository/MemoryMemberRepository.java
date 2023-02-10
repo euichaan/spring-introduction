a9b2a753ea0720e4bf5d10a8715401db5c2ba844
euichanhwang <pine6971@naver.com>
2023-02-10 18:23:17 +0900

    feat : 컴포넌트 스캔으로 자동 의존관계를 설정한다.

--- src/main/java/com/me/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/com/me/hellospring/repository/MemoryMemberRepository.java
@@ -6,10 +6,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import org.springframework.stereotype.Repository;
+
 import com.me.hellospring.domain.Member;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
-	private static Map<Long, Member> store = new HashMap<>();
+	private static final Map<Long, Member> store = new HashMap<>();
 	private static long sequence = 0L;
 
 	@Override
