22d9ee96b7e1bbc90bc6b32fb744d0c37ef3fb1e
euichanhwang <pine6971@naver.com>
2023-02-12 14:43:18 +0900

    feat : 스프링 JdbcTemplate를 이용해 데이터베이스에 접속한다.

--- src/main/java/com/me/hellospring/SpringConfig.java
+++ src/main/java/com/me/hellospring/SpringConfig.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.me.hellospring.repository.JdbcMemberRepository;
+import com.me.hellospring.repository.JdbcTemplateMemberRepository;
 import com.me.hellospring.repository.MemberRepository;
 import com.me.hellospring.service.MemberService;
 
@@ -27,6 +27,6 @@ public class SpringConfig {
 
 	@Bean
 	public MemberRepository memberRepository() {
-		return new JdbcMemberRepository(dataSource);
+		return new JdbcTemplateMemberRepository(dataSource);
 	}
 }
